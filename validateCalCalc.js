a8add54d5ee04efddf722f23a29c51ccd578d12c
iamsouthernbear <lasouthernbear@gmail.com>
2017-03-31 03:09:02 +0300

    Add names for variables. Add classes for сorrect indication feedback. Add function isInvalid. And did some other shit)

--- validateCalCalc.js
+++ validateCalCalc.js
@@ -1,59 +1,72 @@
 $(document).ready(function(){
+
+  const weight = $('#weight input');
+  const height = $('#height input');
+  const age = $('#age input');
+  let result;
+
+  function isInvalid() {
+    return height.val() < 1 ||
+        isNaN(height.val()) ||
+        weight.val() < 1    ||
+        isNaN(weight.val()) ||
+        !age.val();
+  };
   //Функция для индикации правильно введенных данных
   const indicateSuccess = function success(divID) {
     $(divID).attr('class', 'form-group has-success has-feedback');
-    $(divID+' :nth-child(4)').attr('class', 'glyphicon glyphicon-ok form-control-feedback');
+    $(divID+' .error').attr('class', 'error hide');
+    $(divID+' .success').attr('class', 'glyphicon glyphicon-ok form-control-feedback success');
   };
   //Функция для индикации не верных данных
   const indicateError = function error(divID) {
     $(divID).attr('class', 'form-group has-error has-feedback');
-    $(divID+' :nth-child(4)').attr('class', 'glyphicon glyphicon-remove form-control-feedback');
+    $(divID+' .success').attr('class', 'success hide');
+    $(divID+' .error').attr('class', 'glyphicon glyphicon-remove form-control-feedback error');
   };
   //Валидация формы с индикацией
   $('form').submit(function(e){
 
-    if ($('#weight input').val() < 1 || isNaN($('#weight input').val())) {
+    if (weight.val() < 1 || isNaN(weight.val())) {
       indicateError('#weight');
     } else {
       indicateSuccess('#weight');
     };
 
-    if ($('#growth input').val() < 1 || isNaN($('#growth input').val())) {
-      indicateError('#growth');
+    if (height.val() < 1 || isNaN(height.val())) {
+      indicateError('#height');
     } else {
-      indicateSuccess('#growth');
+      indicateSuccess('#height');
     };
 
-    if ($('#age input').val()) {
+    if (age.val()) {
       indicateSuccess('#age');
     } else {
       indicateError('#age');
     };
 
     e.preventDefault();
-  });
-  //Выводим модалку
-  $('#but').click(function(){
-    if ($('#growth input').val() < 1 || isNaN($('#growth input').val()) || $('#weight input').val() < 1 || isNaN($('#weight input').val()) || !$('#age input').val()) {
-      return;
-    };
 
-    if ($('input[name="sex"]:checked').val() === 'female') {
-      let resultFemale = Math.round(655.1+9.563*$('#weight input').val()+1.85*$('#growth input').val()-4.676*$('#age input').val());
-      $('.modal-body').append(resultFemale + ' kkal');
+    if (isInvalid()) {
+      return;
     } else {
-      let resultMale = Math.round(66.5+13.75*$('#weight input').val()+5.003*$('#growth input').val()-6.775*$('#age input').val());
-      $('.modal-body').append(resultMale + ' kkal');
+      if ($('input[name="sex"]:checked').val() === 'female') {
+        result = Math.round(655.1+9.563*weight.val()+1.85*height.val()-4.676*age.val());
+      } else {
+        result = Math.round(66.5+13.75*weight.val()+5.003*height.val()-6.775*age.val());
+      };
+      $('.modal-body').append(result + ' kkal');
     };
 
     $('#myModal').modal('show');
   });
+
   //Очищаем форму и модалку
   $('#myModal').on('hidden.bs.modal', function(){
     $('form')[0].reset();
     $('.modal-body').html('');
-    $('#weight, #growth, #age').attr('class', 'form-group');
-    $('#weight :nth-child(4), #growth :nth-child(4), #age :nth-child(4)').attr('class', 'hidden');
+    $('#weight, #height, #age').attr('class', 'form-group');
+    $('#weight .success, #height .success, #age .success').attr('class', 'success hide');
   });
 
 });
